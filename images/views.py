1630e393a795b3a1d16f4a1481b572cf51c75b58
Owilly <pasaclouma54@gmail.com>
2022-03-25 21:22:26 +0300

    start images app, configure it and connected it to views

---
+++ images/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gallery(request):
+    return render(request, 'images/gallery.html')
+
+def viewImage(request):
+    return render(request, 'images/image.html')
+
+def addImage(request):
+    return render(request, 'images/add_image.html')
