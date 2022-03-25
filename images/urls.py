d09d48f61aa4fd79037e682fc73d72648e5831e1
Owilly <pasaclouma54@gmail.com>
2022-03-25 22:06:46 +0300

    add urlpatterns in the gallery urls

--- images/urls.py
+++ images/urls.py
@@ -1,1 +1,8 @@
-       
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.gallery, name='gallery')
+    path('image/<str:pk>', views.views.viewImage, name='image'),
+    path('addImage', views.add_image, name='addImage'),
+]           
