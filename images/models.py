a2cd939ebc3edc56664001aa20f01dbd917d25b5
Owilly <pasaclouma54@gmail.com>
2022-03-26 09:12:32 +0300

    create model classes and connect to the database

--- images/models.py
+++ images/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Category(models.Model):
+    name = CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+    
