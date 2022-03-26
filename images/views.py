6b4f4b1714ac42fb708e5d2de322f6bc603c4141
Owilly <pasaclouma54@gmail.com>
2022-03-26 10:10:28 +0300

    add categories of the images

--- images/views.py
+++ images/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Image
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'images/gallery.html')
+    categories = Category.objects.all()
+    context = {'categories' : categories}
+    return render(request, 'images/gallery.html', context)
 
 def viewImage(request, pk):
     return render(request, 'images/image.html')
