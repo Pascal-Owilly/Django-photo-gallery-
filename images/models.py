7e3dee8556afe78837b86b2a8c8bde8754f8b39e
Owilly <pasaclouma54@gmail.com>
2022-03-26 13:31:20 +0300

    add photo categories

--- images/models.py
+++ images/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.forms import CharField
+
 
 # Create your models here.
 class Category(models.Model):
-    name = CharField(max_length=200)
+    name = models.CharField(max_length=150, null=False, blank=False)
 
     def __str__(self):
         return self.name
--- images/views.py
+++ images/views.py
@@ -4,11 +4,15 @@ from .models import Category, Image
 # Create your views here.
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories' : categories}
+    images = Image.objects.all()
+    context = {'categories' : categories, 'images' : images}
     return render(request, 'images/gallery.html', context)
 
 def viewImage(request, pk):
-    return render(request, 'images/image.html')
+    image  = Image.objects.get(id=pk)
+    return render(request, 'images/image.html', {'image' : image})
 
 def addImage(request):
     return render(request, 'images/add_image.html')
+ 
+    
