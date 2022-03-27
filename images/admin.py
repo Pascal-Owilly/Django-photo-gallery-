b6fcda2092bb0d292a26f17c38679b2b6f825dd3
Owilly <pasaclouma54@gmail.com>
2022-03-27 21:49:31 +0300

    testing

--- images/admin.py
+++ images/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Category, Image, Location
 
 # Register your models here.
-
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Location)
